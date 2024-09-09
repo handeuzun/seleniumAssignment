d8f272668e5f3f59106474b28560e58e5dd16004
Handenur Uzun <handenur.uzun@getir.com>
2024-09-09 19:51:24 +0300

    TestNG added.

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,18 +1,17 @@
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.TestInstance;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+//@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class BaseTest {
 
     protected WebDriver driver;
     ProductDetailPage productDetailPage ;
     LoginPage loginPage ;
 
-    @BeforeAll
+    @BeforeMethod
     public void setUp(){
         System.setProperty("webdriver.chrome.driver","drivers/chromedriver");
         ChromeOptions options = new ChromeOptions();
@@ -23,7 +22,7 @@ public class BaseTest {
         loginPage = new LoginPage(driver);
     }
 
-    @AfterAll
+    @AfterMethod
     public void tearDown(){
         driver.quit();
     }
